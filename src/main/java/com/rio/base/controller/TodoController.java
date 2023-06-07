586a96e7489f0dd4881674fdc19c269ad3bbe755
rio <sw_park0205@naver.com>
2023-06-07 21:41:29 +0900

    @PostMapping(@ModelAttribute) 적용, 아직 한글미지원으로 한글데이터는 글자가 깨진다.

--- src/main/java/com/rio/base/controller/TodoController.java
+++ src/main/java/com/rio/base/controller/TodoController.java
@@ -2,9 +2,12 @@ package com.rio.base.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.rio.base.dto.TodoDTO;
 
 import lombok.extern.log4j.Log4j2;
 
@@ -21,12 +24,13 @@ public class TodoController {
 	//@RequestMapping(value = "/register", method = RequestMethod.GET)
 	@GetMapping("/register")
 	public void register() {
-		log.info("todo register.......");
+		log.info("GET todo register.......");
 	}
 	
 	@PostMapping("/register")
-	public void registerPost() {
-		log.info("POST todo register.....");
+	public void registerPost(@ModelAttribute TodoDTO todoDTO) {
+		log.info("@ModelAttribute POST todo register.....");
+		log.info(todoDTO);
 	}
 
 }
